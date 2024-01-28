d85d3160c97828c9bd27af6e79cdc2b3513fbabb
yangerhang <yangerhang@wps.cn>
2024-01-28 21:08:11 +0800

    为llama.cmake增加新的基础功能，并且完善文档自动化
    新增：
    1. LLAMA_WIN，LLAMA_UNIX、LLAMA_LINUX  C++平台宏。在目标平台为对应平台时定义。
    2. 自动添加源文件的机制。调用scripts\update_filelist.py能自动生成sources.cmake，里面包含的源文件和头文件列表会被自动读取。开发者每次添加新的文件时都应当运行该脚本，来更新这个文件。
    3. 引入doxygen美化子仓库

--- projects/llama/cmake/llama.cmake
+++ projects/llama/cmake/llama.cmake
@@ -18,48 +18,108 @@
 # 允许的源文件和头文件扩展名
 set(LLAMA_SOURCE_EXTENSIONS "*.cpp" "*.c" CACHE INTERNAL "source file extensions that are allowed within the llama project")
 set(LLAMA_HEADER_EXTENSIONS "*.hpp" "*.h" CACHE INTERNAL "header file extensions that are allowed within the llama project")
+unset(llama_doc_sources_sp CACHE)
+unset(llama_doc_sources_ls CACHE)
 
 # 自动创建 llama 目标。
-# 在创建目标前，先用scripts\update_filelist.py生成或者更新源文件列表sources.cmake。
+# llama项目的组织原则是：用固定的文件树约定代替繁琐重复的cmake配置。所有目标都必须具有相同的目录结构：
+# src - 目标的私有源文件和头文件目录：存放源文件和不让下游包含的头文件。
+# include - 目标的公有头文件。存放下游需要的头文件。
+# test - 测试目标。作为主目标的一个下游目标，链接到主目标。必须存在。
+# 
+# 在创建目标前，先用scripts\update_filelist.py生成源文件列表sources.cmake。
 # 该文件应当和源码一起检入git。
-# 参数：
+# 必要参数：
 # name - 目标的名称。
 # type - 目标类型。目前支持的目标有 HEADER_ONLY 、SHARED 和 EXECUTABLE
+# 可选参数:
+# AKA <aka>: 模块缩写。建议不超过5个字母。对于动态库，会加入私有预处理器定义 LLAMA_{大写别名}_EXPORT 。默认为模块全名。
 function(llama_target name type)
-	# p.s. CMAKE_CURRENT_LIST_DIR 是调用者的位置，不是本文件的位置
-	llama_internal_include_source_list(SOURCE_LIST TEST_SOURCE_LIST)
-	if(type STREQUAL HEADER_ONLY)
-		add_library("${name}" INTERFACE)
-		target_include_directories("${name}" INTERFACE "${CMAKE_CURRENT_LIST_DIR}/include")
+	cmake_parse_arguments(
+		"ARG" # prefix
+		"" # option
+		"AKA" # one-val
+		"" # multi-val
+		${ARGN}
+	)
+
+	if(WIN32)
+		list(APPEND PLAT_DEF "LLAMA_WIN")
 	else()
+		if(UNIX)
+			list(APPEND PLAT_DEF "LLAMA_UNIX")
+		endif()
+		
+		if(LINUX)
+			list(APPEND PLAT_DEF "LLAMA_LINUX")
+		endif()
+	endif()
+
+	if(NOT ARG_AKA)
+		set(ARG_AKA ${name})
+	endif()
+	string(TOUPPER ${ARG_AKA} AKA_UPPER)
+
+	# p.s. CMAKE_CURRENT_LIST_DIR 是调用者的位置，不是本文件的位置
+	llama_internal_verify_target("${name}" "${type}")
+	llama_internal_include_source_list(SOURCE_LIST TEST_SOURCE_LIST)
+
+	foreach(src ${SOURCE_LIST})
+		set(llama_doc_sources_sp "${CMAKE_CURRENT_LIST_DIR}/${src} $CACHE{llama_doc_sources_sp}"
+			CACHE INTERNAL "space-separated list of source files that need to be documented")
+		set(llama_doc_sources_ls "${CMAKE_CURRENT_LIST_DIR}/${src};$CACHE{llama_doc_sources_ls}"
+			CACHE INTERNAL "semi-column-separated list of source files that need to be documented")
+	endforeach()
+	
+	if(type STREQUAL HEADER_ONLY)
+		set(PUB_VIS "INTERFACE")
+		add_library("${name}" INTERFACE)
+	else()
+		set(PUB_VIS "PUBLIC")
 		if(type STREQUAL SHARED)
-			add_library("${name}-obj" OBJECT "${SOURCE_LIST}")
-			add_library("${name}" SHARED "$<TARGET-objECTS:${name}-obj>")
-		elseif(type STREQUAL EXECUTABLE)
-			add_library("${name}-obj" OBJECT)
-			add_executable("${name}" "$<TARGET-objECTS:${name}-obj>")
+			add_library("${name}" SHARED "${SOURCE_LIST}")
+			target_compile_definitions("${name}" PRIVATE "LLAMA_${AKA_UPPER}_EXPORT")
+		elseif(type STREQUAL EXECUTABLE)  
+			add_executable("${name}" "${SOURCE_LIST}")
 		else()
 			message(FATAL_ERROR "unknown type: ${type}")
-		endif()		
-		target_include_directories("${name}-obj" PUBLIC "${CMAKE_CURRENT_LIST_DIR}/include")
-		message(DEBUG  "added include for ${name}-obj : ${CMAKE_CURRENT_LIST_DIR}/include")
+		endif()
+		target_include_directories("${name}" PRIVATE "${CMAKE_CURRENT_LIST_DIR}/src")
 	endif()
+
+	target_include_directories("${name}" ${PUB_VIS} "${CMAKE_CURRENT_LIST_DIR}/include")
+	target_compile_definitions("${name}" ${PUB_VIS} "${PLAT_DEF}")
+
 	add_executable("${name}-test" "${TEST_SOURCE_LIST}")
 	target_link_libraries("${name}-test" PUBLIC "${name}" GTest::gtest GTest::gtest_main)
 	add_test(NAME "${name}-test" COMMAND "${name}-test")
-	llama_internal_verify_target("${name}" "${type}")
+
+	# Protobuf
+	# foreach(proto_src ${TEST_PROTO_LIST} ${PROTO_LIST})
+	# 	set(PROTO_OUTPUT_PATH )
+	# 	add_custom_command(
+	# 		OUTPUT
+	# 	)
+	# 	target_link_libraries("${name}-obj" PUBLIC protobuf::libprotobuf protobuf::libprotoc)
+	# endforeach()
+	
+
 endfunction()
 
 function(llama_docs)
-	set(INPUT_DIR "${CMAKE_CURRENT_FUNCTION_LIST_DIR}/../") # Doxygen 输入目录，也就是要文档化的源码路径
-	set(OUTPUT_DIR "${CMAKE_CURRENT_BINARY_DIR}/docs") # Doxygen 输出目录，文档输出在这里
+	set(OUTPUT_DIR "${CMAKE_BINARY_DIR}/docs") # Doxygen 输出目录，文档输出在这里
 	set(DOXYFILE_INPUT_PATH "${CMAKE_CURRENT_FUNCTION_LIST_DIR}/../docs/llama.doxyfile") # 输入的 doxyfile 模板
-	set(DOXYFILE_OUTPUT_PATH "${CMAKE_CURRENT_BINARY_DIR}/docs/Doxyfile") # 输出的 Doxyfile 文件，文件里的路径被替换了
+	set(DOXYFILE_OUTPUT_PATH "${CMAKE_BINARY_DIR}/docs/Doxyfile") # 输出的 Doxyfile 文件，文件里的路径被替换了
+	set(DOXYGEN_STYLE_SHEET_PATH "${CMAKE_CURRENT_FUNCTION_LIST_DIR}/../../../doxygen-awesome-css/doxygen-awesome.css")
+
+	file(TO_CMAKE_PATH "${DOXYGEN_STYLE_SHEET_PATH}" DOXYGEN_STYLE_SHEET_PATH)
 	string(REPLACE ";" " " "INPUT_FILE_PATTERNS" "${LLAMA_SOURCE_EXTENSIONS};${LLAMA_HEADER_EXTENSIONS}")
 	file(MAKE_DIRECTORY "${OUTPUT_DIR}")
 
 	option(LLAMA_BUILD_DOCS "Build documentation" OFF)
+	
 	if(LLAMA_BUILD_DOCS)
+		message("Building docs")
 		# 无法找到Doxygen。如果不希望生成文档，请在cmake命令行增加 -DLLAMA_BUILD_DOCS=OFF
 		find_package(Doxygen REQUIRED)
 
@@ -70,11 +130,12 @@ function(llama_docs)
 				"${CMAKE_COMMAND}"
 				"-DDOXYFILE_INPUT_PATH=${DOXYFILE_INPUT_PATH}"
 				"-DDOXYFILE_OUTPUT_PATH=${DOXYFILE_OUTPUT_PATH}"
-				"-DINPUT_DIR=${INPUT_DIR}"
+				"-DINPUT=$CACHE{llama_doc_sources_sp}"
 				"-DOUTPUT_DIR=${OUTPUT_DIR}"
-				"-DINPUT_FILE_PATTERNS=${INPUT_FILE_PATTERNS}"
+				"-DCLANG_DATABASE_PATH=${CMAKE_BINARY_DIR}/compile_commands.json"
+				"-DDOXYGEN_STYLE_SHEET_PATH=${DOXYGEN_STYLE_SHEET_PATH}"
 				"-P"
-				"${CMAKE_CURRENT_FUNCTION_LIST_DIR}/llama_configure.cmake"
+				"${CMAKE_CURRENT_FUNCTION_LIST_DIR}/llama_configure_doxygen.cmake"
 			WORKING_DIRECTORY "${OUTPUT_DIR}"
 			DEPENDS "${DOXYFILE_INPUT_PATH}"
 			COMMENT "Configuring doxyfile"
@@ -91,14 +152,15 @@ function(llama_docs)
 			COMMAND echo "delete me to trigger a rebuild of the docs" > "${OUTPUT_DIR}/docs.stamp"
 			COMMAND "${CMAKE_COMMAND}" -E create_symlink "${OUTPUT_DIR}/html/index.html" "${OUTPUT_DIR}/llama-documentation.html"
 			WORKING_DIRECTORY "${OUTPUT_DIR}"
-			DEPENDS "${DOXYFILE_OUTPUT_PATH}" "docs-configure" # add_custom_command 的 depend 可以是文件或者 target
+			DEPENDS "${DOXYFILE_OUTPUT_PATH}" "docs-configure" ${llama_doc_sources_ls} # add_custom_command 的 depend 可以是文件或者 target
 			COMMENT "Generating docs"
 			VERBATIM)
 		add_custom_target(
 			docs
 			DEPENDS "${OUTPUT_DIR}/docs.stamp" # add_custom_target 的 depend 仅限文件且必须是同一个 CMakeLists.txt
 		)
-	endif()
+
+	endif() 
 endfunction()
 
 function(llama_internal_include_source_list src_out test_src_out)
@@ -145,6 +207,9 @@ function(llama_internal_verify_target name type)
 	option(LLAMA_IGNORE_TARGET_VERIFICATION OFF)
 
 	if(NOT CACHE{LLAMA_IGNORE_TARGET_VERIFICATION})
+		if(NOT EXISTS "${CMAKE_CURRENT_LIST_DIR}/sources.cmake")
+			message(FATAL_ERROR "${CMAKE_CURRENT_LIST_DIR}/sources.cmake does not exist\nuse scripts/generate_file_list.py to generate one.")
+		endif()
 		llama_internal_verify_dir(PATH "${CMAKE_CURRENT_LIST_DIR}/include")
 		llama_internal_verify_dir(PATH "${CMAKE_CURRENT_LIST_DIR}/include/${name}" MUST_CONTAIN ${LLAMA_HEADER_EXTENSIONS})
 		llama_internal_verify_dir(PATH "${CMAKE_CURRENT_LIST_DIR}/test" MUST_CONTAIN ${LLAMA_SOURCE_EXTENSIONS})
@@ -170,7 +235,7 @@ function(llama_internal_verify_target name type)
 		llama_internal_normalize_list(actual_src)
 
 		if(NOT src STREQUAL actual_src)
-			message(WARNING "sources.cmake for target '${name}' may not be up to date because it differs from the source tree.\nthe file: ${src}\nthe source tree: ${actual_src}\n")
+			message(WARNING "sources.cmake for target '${name}' may not be up to date because it differs from the source tree.\nthe file: ${src}\nthe source tree: ${actual_src}\nuse scripts/generate_file_list.py to update it.")
 		endif()
 
 	endif()
--- projects/llama/scripts/generate_file_list.py
+++ projects/llama/scripts/generate_file_list.py
@@ -1,12 +1,18 @@
 import os, sys
 import glob
 
-def is_source_file(path):
-	suffixes = ['.cpp', '.h', '.hpp', '.c']
+def ends_with(path, suffixes):
 	for s in suffixes:
 		if path.endswith(s):
 			return True
 	return False
+
+def get_file_type(path):
+	if ends_with(path, ['.cpp', '.h', '.hpp', '.c']):
+		return 'SOURCE'
+	elif ends_with(path, ['proto']):
+		return 'PROTO'
+	return None
 
 def traverse_files(path):
 	output_path = os.path.join(path, "sources.cmake")
@@ -14,22 +20,32 @@ def traverse_files(path):
 	src = glob.glob(os.path.join(path, "src/**"), recursive=True)
 	include = glob.glob(os.path.join(path, "include/**"), recursive=True) 
 	test = glob.glob(os.path.join(path, "test/**"), recursive=True)
+	
 	for filename in src + include:
-		if is_source_file(filename):
-			filename = os.path.relpath(filename, path)
-			filename = filename.replace("\\", "/")
-			print(filename)
-			output_file.write(f'list(APPEND SOURCE_LIST "{filename}")\n')
+		file_type = get_file_type(filename)
+		if file_type is None:
+			continue
+		filename = os.path.relpath(filename, path)
+		filename = filename.replace("\\", "/")
+		print(filename)
+		output_file.write(f'list(APPEND {file_type}_LIST "{filename}")\n')
+
 	for filename in test:
-		if is_source_file(filename):
-			filename = os.path.relpath(filename, path)
-			filename = filename.replace("\\", "/")
-			print(filename)
-			output_file.write(f'list(APPEND TEST_SOURCE_LIST "{filename}")\n')
+		file_type = get_file_type(filename)
+		if file_type is None:
+			continue
+		filename = os.path.relpath(filename, path)
+		filename = filename.replace("\\", "/")
+		print(filename)
+		output_file.write(f'list(APPEND TEST_{file_type}_LIST "{filename}")\n')
 
 if __name__ == "__main__":
-	if len(sys.argv) <= 2:
-		input_dir = input("Type source directory of your module: ")
-	else:
-		input_dir = sys.argv[1]
-	traverse_files(input_dir)
+	# llama 里面有些是cpp模块，有些不是。把不是的排除掉。
+	exclude_list = ('cmake', 'docs', 'scripts')
+	# projects/llama
+	project_root = os.path.abspath(os.path.join(sys.argv[0], '..', '..'))
+	for dir_name in os.listdir(project_root):
+		# e.g. projects/llama/cmake
+		dir_path = os.path.join(project_root, dir_name)
+		if os.path.isdir(dir_path) and dir_name not in exclude_list:
+			traverse_files(dir_path)
