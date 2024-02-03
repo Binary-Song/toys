54c0aef50ee50a914ab06e29215aa2f8930e9b8f
yangerhang <yangerhang@wps.cn>
2024-02-03 18:26:46 +0800

    添加opengl

---
+++ projects/llama/opengl_loader/sources.cmake
@@ -0,0 +1,4 @@
+list(APPEND SOURCE_LIST "src/glad.c")
+list(APPEND SOURCE_LIST "include/glad/glad.h")
+list(APPEND SOURCE_LIST "include/KHR/khrplatform.h")
+list(APPEND TEST_SOURCE_LIST "test/dummy_test.cpp")
