0030a46062372efdf0199bff8e21db0398f3881e
yangerhang <yangerhang@kingsoft.com>
2024-02-19 09:27:19 +0800

    将 llama_target 重命名为 llama_module

--- projects/llama/cmake/llama.cmake
+++ projects/llama/cmake/llama.cmake
@@ -18,49 +18,53 @@ set(LLAMA_HEADER_EXTENSIONS "*.hpp" "*.h" CACHE INTERNAL "header file extensions
 unset(llama_doc_sources_sp CACHE)
 unset(llama_doc_sources_ls CACHE)
 
-# 自动创建 llama 目标。
+# 自动创建 llama 模块。“模块”包含多个自动创建的目标。
 # llama项目的组织原则是：用固定的文件树约定代替繁琐重复的cmake配置。所有目标都必须具有相同的目录结构：
 # src - 目标的私有源文件和头文件目录：存放源文件和不让下游包含的头文件。
 # include - 目标的公有头文件。存放下游需要的头文件。
 # test - 测试目标。作为主目标的一个下游目标，链接到主目标。必须存在。
 # 
-# 在创建目标前，先用scripts\update_filelist.py生成源文件列表sources.cmake。
+# 在创建模块前，先用scripts\update_filelist.py生成源文件列表sources.cmake。
 # 该文件应当和源码一起检入git。
 # 必要参数：
-# name - 目标的名称。
+# name - 模块的名称。模块对应的目标名以它为前缀。
 # type - 目标类型。目前支持的目标有 HEADER_ONLY 、SHARED 和 EXECUTABLE
 # 可选参数:
 # AKA <aka>: 模块缩写。建议不超过5个字母。对于动态库，会加入私有预处理器定义 LLAMA_{大写别名}_EXPORT 。默认为模块全名。
-function(llama_target name type)
+function(llama_module name type)
 	cmake_parse_arguments(
 		"ARG" # prefix
 		"" # option
 		"AKA" # one-val
-		"" # multi-val
+		"DEPENDS" # multi-val
 		${ARGN}
 	)
-
+	# 定义平台宏
 	if(WIN32)
 		list(APPEND PLAT_DEF "LLAMA_WIN")
 	else()
 		if(UNIX)
 			list(APPEND PLAT_DEF "LLAMA_UNIX")
 		endif()
-		
 		if(LINUX)
 			list(APPEND PLAT_DEF "LLAMA_LINUX")
 		endif()
 	endif()
 
+	# 设置 AKA 的默认值 （name）
 	if(NOT ARG_AKA)
 		set(ARG_AKA ${name})
 	endif()
 	string(TOUPPER ${ARG_AKA} AKA_UPPER)
 
+	# 验证项目结构的正确性，包括目录结构是不是存在等
 	# p.s. CMAKE_CURRENT_LIST_DIR 是调用者的位置，不是本文件的位置
 	llama_internal_verify_target("${name}" "${type}")
+	
+	# include 文件 source.cmake
 	llama_internal_include_source_list()
 
+	# llama_doc_sources_sp 和 llama_doc_sources_ls 都是 文档的输入文件列表，不过前者用空格切分，后者用分号
 	foreach(src ${SOURCE_LIST})
 		set(llama_doc_sources_sp "${CMAKE_CURRENT_LIST_DIR}/${src} $CACHE{llama_doc_sources_sp}"
 			CACHE INTERNAL "space-separated list of source files that need to be documented")
@@ -68,30 +72,71 @@ function(llama_target name type)
 			CACHE INTERNAL "semi-column-separated list of source files that need to be documented")
 	endforeach()
 	
+	# PUB_VIS：
+	#  在 HEADER_ONLY 时等于 INTERFACE
+	#  在其他情况等于 PUBLIC
+	# 因为 INTERFACE 库只允许 INTERFACE 可见性，所以只能出此下策。
 	if(type STREQUAL HEADER_ONLY)
 		set(PUB_VIS "INTERFACE")
-		add_library("${name}" INTERFACE)
 	else()
 		set(PUB_VIS "PUBLIC")
-		if(type STREQUAL SHARED)
-			add_library("${name}" SHARED "${SOURCE_LIST}")
-			target_compile_definitions("${name}" PRIVATE "LLAMA_${AKA_UPPER}_EXPORT")
-		elseif(type STREQUAL EXECUTABLE)  
-			add_executable("${name}" "${SOURCE_LIST}")
-		elseif(type STREQUAL STATIC)
-			add_library("${name}" STATIC "${SOURCE_LIST}")
-		else()
-			message(FATAL_ERROR "unknown type: ${type}")
-		endif()
+	endif()
+
+	# 因为 cmake 要求 target至少要有一个源文件。所以弄一个凑数的。
+	set(DUMMY_PATH "${CMAKE_CURRENT_FUNCTION_LIST_DIR}/dummy.cpp")
+
+	# ============== 各个 type 不一样的逻辑写这里 ==============
+	# SHARED 类型：
+	#  包括如下目标：
+	#    ${name}-static  静态库
+	#    ${name}         动态库，链接 ${name}-static
+	#    ${name}-test    测试库，链接 ${name}-test
+	# EXECUTABLE 类型：
+	#  包括如下目标：
+	#    ${name}-static  静态库
+	#    ${name}         可执行，链接 ${name}-static
+	#    ${name}-test    测试库，链接 ${name}-test
+	if(type STREQUAL SHARED)
+		add_library("${name}-static" STATIC "${SOURCE_LIST}")
+		add_library("${name}" SHARED "${DUMMY_PATH}")
+		target_link_libraries("${name}" PRIVATE "${name}-static")
+		target_compile_definitions("${name}-static" PRIVATE "LLAMA_${AKA_UPPER}_EXPORT")
+	elseif(type STREQUAL EXECUTABLE)
+		add_library("${name}-static" STATIC "${SOURCE_LIST}")
+		add_executable("${name}" "${DUMMY_PATH}")
+		target_link_libraries("${name}" PRIVATE "${name}-static")
+	elseif(type STREQUAL HEADER_ONLY)
+		add_library("${name}" INTERFACE)
+	elseif(type STREQUAL STATIC)
+		add_library("${name}" STATIC "${SOURCE_LIST}")
+	else()
+		message(FATAL_ERROR "unknown type: ${type}")
+	endif() 
+
+	# ============== 除了 HEADER_ONLY 外，其他 type 都一样的逻辑写这里 (HEADER_ONLY 真的很特殊) ==============
+	if(type STREQUAL HEADER_ONLY)
+	else()
 		target_include_directories("${name}" PRIVATE "${CMAKE_CURRENT_LIST_DIR}/src")
 	endif()
 
+	# ============== 所有 type 一样的逻辑写这里,包括 HEADER_ONLY ==============
 	target_include_directories("${name}" ${PUB_VIS} "${CMAKE_CURRENT_LIST_DIR}/include")
 	target_compile_definitions("${name}" ${PUB_VIS} "${PLAT_DEF}")
 
 	add_executable("${name}-test" "${TEST_SOURCE_LIST}")
-	target_link_libraries("${name}-test" PUBLIC "${name}" GTest::gtest GTest::gtest_main)
+	target_link_libraries("${name}-test" ${PUB_VIS} "${name}" GTest::gtest GTest::gtest_main)
 	add_test(NAME "${name}-test" COMMAND "${name}-test")
+
+	# 处理 DEPENDS
+	foreach(depend ${ARG_DEPENDS})
+		target_link_libraries("${name}" ${PUB_VIS} "${depend}")
+	endforeach()
+
+	# 处理 protobuf 
+	if(PROTO_LIST OR TEST_PROTO_LIST)
+		llama_internal_add_protobuf_targets()
+	endif()
+	
 	unset(proto_output_list)
 	foreach(src ${TEST_PROTO_LIST} ${PROTO_LIST}) 
 		set(proto_input_path "${CMAKE_CURRENT_LIST_DIR}/${src}")
@@ -126,9 +171,11 @@ function(llama_docs)
 	set(OUTPUT_DIR "${CMAKE_BINARY_DIR}/docs") # Doxygen 输出目录，文档输出在这里
 	set(DOXYFILE_INPUT_PATH "${CMAKE_CURRENT_FUNCTION_LIST_DIR}/../docs/llama.doxyfile") # 输入的 doxyfile 模板
 	set(DOXYFILE_OUTPUT_PATH "${CMAKE_BINARY_DIR}/docs/Doxyfile") # 输出的 Doxyfile 文件，文件里的路径被替换了
-	set(DOXYGEN_STYLE_SHEET_PATH "${CMAKE_CURRENT_FUNCTION_LIST_DIR}/../docs/doc-css/doxygen-awesome.css")
-
-	file(TO_CMAKE_PATH "${DOXYGEN_STYLE_SHEET_PATH}" DOXYGEN_STYLE_SHEET_PATH)
+	option(USE_STYLESHEET OFF)
+	if(USE_STYLESHEET)
+		set(DOXYGEN_STYLE_SHEET_PATH "${CMAKE_CURRENT_FUNCTION_LIST_DIR}/../docs/doc-css/doxygen-awesome.css")
+		file(TO_CMAKE_PATH "${DOXYGEN_STYLE_SHEET_PATH}" DOXYGEN_STYLE_SHEET_PATH)
+	endif()
 	string(REPLACE ";" " " "INPUT_FILE_PATTERNS" "${LLAMA_SOURCE_EXTENSIONS};${LLAMA_HEADER_EXTENSIONS}")
 	file(MAKE_DIRECTORY "${OUTPUT_DIR}")
 
@@ -258,3 +305,7 @@ function(llama_internal_normalize_list list)
 	list(SORT "${list}")
 	set("${list}" "${${list}}" PARENT_SCOPE)
 endfunction()
+
+macro(llama_internal_add_protobuf_targets)
+	
+endmacro()
