be0ec0b3f67faef73b8091bb1ddaf131e79ee5b1
yangerhang <yangerhang@kingsoft.com>
2024-02-22 10:14:39 +0800

    暂时禁用path

--- projects/llama/foundation/src/path.cpp
+++ projects/llama/foundation/src/path.cpp
@@ -1,93 +1,93 @@
-#include "foundation/path.h"
-#include "foundation/enums.h"
-#include <cctype>
+// #include "foundation/path.h"
+// #include "foundation/enums.h"
+// #include <cctype>
 
-namespace llama
-{
+// namespace llama
+// {
 
-static bool IsSep(char ch)
-{
-#ifdef LLAMA_WIN
-    return ch == '/' || ch == '\\';
-#endif
-    return ch == '/';
-}
+// static bool IsSep(char ch)
+// {
+// #ifdef LLAMA_WIN
+//     return ch == '/' || ch == '\\';
+// #endif
+//     return ch == '/';
+// }
 
-static bool IsValidRelativePath(std::string_view path)
-{
-#ifdef LLAMA_WIN
-    // C:\ 或者 C:/
-    if (path.size() >= 3 && tolower(path[0]) >= 'a' && tolower(path[0]) <= 'z' && path[1] == ':' && IsSep(path[2]))
-    {
-        return false;
-    }
-    // 这个和linux的绝对路径比较像，也ban了
-    if (path.starts_with('\\'))
-    {
-        return false;
-    }
-#else
-    if (path.starts_with('/'))
-    {
-        return false;
-    }
-#endif
-    return true;
-}
+// static bool IsValidRelativePath(std::string_view path)
+// {
+// #ifdef LLAMA_WIN
+//     // C:\ 或者 C:/
+//     if (path.size() >= 3 && tolower(path[0]) >= 'a' && tolower(path[0]) <= 'z' && path[1] == ':' && IsSep(path[2]))
+//     {
+//         return false;
+//     }
+//     // 这个和linux的绝对路径比较像，也ban了
+//     if (path.starts_with('\\'))
+//     {
+//         return false;
+//     }
+// #else
+//     if (path.starts_with('/'))
+//     {
+//         return false;
+//     }
+// #endif
+//     return true;
+// }
 
-static bool IsValidAbsolutePath(std::string_view path)
-{
-#ifdef LLAMA_WIN
-    // C:\ 或者 C:/
-    if (path.size() >= 3 && tolower(path[0]) >= 'a' && tolower(path[0]) <= 'z' && path[1] == ':' && IsSep(path[2]))
-    {
-        return true;
-    }
-#else
-    if (path.starts_with('/'))
-    {
-        return true;
-    }
-#endif
-    return false;
-}
+// static bool IsValidAbsolutePath(std::string_view path)
+// {
+// #ifdef LLAMA_WIN
+//     // C:\ 或者 C:/
+//     if (path.size() >= 3 && tolower(path[0]) >= 'a' && tolower(path[0]) <= 'z' && path[1] == ':' && IsSep(path[2]))
+//     {
+//         return true;
+//     }
+// #else
+//     if (path.starts_with('/'))
+//     {
+//         return true;
+//     }
+// #endif
+//     return false;
+// }
 
-RelativePath::RelativePath(std::string_view path, ReadPathOption opt)
-{
-    if (!IsValidRelativePath(path))
-        throw Exception(ExceptionKind::InvalidRelativePath);
+// RelativePath::RelativePath(std::string_view path, ReadPathOption opt)
+// {
+//     if (!IsValidRelativePath(path))
+//         throw Exception(ExceptionKind::InvalidRelativePath);
 
-    std::string component;
-    for (size_t index = 0; index < path.size(); index++)
-    {
-        char ch = path[index];
-        if (!IsSep(ch))
-        {
-            component.push_back(ch);
-        }
-        else
-        {
-            if (!component.empty())
-            {
-                // 检查是否允许 ".." 路径
-                if (component == ".." && !(opt & ReadPathOption::AllowDotDot))
-                {
-                    throw Exception(ExceptionKind::InvalidRelativePath);
-                }
-                m_components.push_back(std::move(component));
-            }
-        }
-    }
-    if (!component.empty())
-    {
-        m_components.push_back(std::move(component));
-    }
-}
+//     std::string component;
+//     for (size_t index = 0; index < path.size(); index++)
+//     {
+//         char ch = path[index];
+//         if (!IsSep(ch))
+//         {
+//             component.push_back(ch);
+//         }
+//         else
+//         {
+//             if (!component.empty())
+//             {
+//                 // 检查是否允许 ".." 路径
+//                 if (component == ".." && !(opt & ReadPathOption::AllowDotDot))
+//                 {
+//                     throw Exception(ExceptionKind::InvalidRelativePath);
+//                 }
+//                 m_components.push_back(std::move(component));
+//             }
+//         }
+//     }
+//     if (!component.empty())
+//     {
+//         m_components.push_back(std::move(component));
+//     }
+// }
 
-Path::Path(std::string_view path, ReadPathOption opt)
-{
-    if (!IsValidAbsolutePath(path))
-        throw Exception(ExceptionKind::InvalidRelativePath);
-}
+// Path::Path(std::string_view path, ReadPathOption opt)
+// {
+//     if (!IsValidAbsolutePath(path))
+//         throw Exception(ExceptionKind::InvalidRelativePath);
+// }
 
-} // namespace llama
+// } // namespace llama
