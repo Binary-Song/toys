521a25929e48045982604a9cd95bc94060f78ae1
yangerhang <yangerhang@kingsoft.com>
2024-02-22 10:12:35 +0800

    lru_cache增加overflow和underflow虚函数和开源协议

--- projects/llama/foundation/include/foundation/lru_cache.h
+++ projects/llama/foundation/include/foundation/lru_cache.h
@@ -1,39 +1,79 @@
-#pragma once
+/*
+
+Copyright (c) 2014, lamerman
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+
+* Neither the name of lamerman nor the names of its
+  contributors may be used to endorse or promote products derived from
+  this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+*/
+
+/*
+ * File:   lrucache.hpp
+ * Author: Alexander Ponomarev
+ *
+ * Created on June 20, 2013, 5:09 PM
+ */
+
+#ifndef _LRUCACHE_HPP_INCLUDED_
+#define _LRUCACHE_HPP_INCLUDED_
+
+#include "exceptions.h"
+#include <cstddef>
 #include <list>
 #include <unordered_map>
 
-template <typename Key, typename Val> class LruCache
+namespace llama
 {
-  public:
-    explicit LruCache(size_t cap) : m_cap(cap)
+
+template <typename key_t, typename value_t> class lru_cache
+{
+public:
+    typedef typename std::pair<key_t, value_t> key_value_pair_t;
+    typedef typename std::list<key_value_pair_t>::iterator list_iterator_t;
+
+    lru_cache(size_t max_size) : _max_size(max_size)
     {
     }
 
-  private:
-    virtual void OverflowAsLruCache()
+    void put(const key_t &key, const value_t &value)
     {
-    }
-
-    virtual void UnderflowAsLruCache()
-    {
-    }
-
-  public:
-    void Put(const Key &key, const Val &value)
-    {
-        ListIter it = m_map.find(key);
-        m_list.push_front({key, value});
-        if (it != m_list.end())
+        auto it = _cache_items_map.find(key);
+        _cache_items_list.push_front(key_value_pair_t(key, value));
+        if (it != _cache_items_map.end())
         {
-            m_list.erase(it->second);
-            m_map.erase(it);
+            _cache_items_list.erase(it->second);
+            _cache_items_map.erase(it);
         }
-        m_map[key] = m_list.begin();
+        _cache_items_map[key] = _cache_items_list.begin();
 
-        if (m_map.size() > m_cap)
+        if (_cache_items_map.size() > _max_size)
         {
-            auto last = m_list.end();
+            auto last = _cache_items_list.end();
             last--;
+            overflow(last->first, last->second);
             _cache_items_map.erase(last->first);
             _cache_items_list.pop_back();
         }
@@ -44,7 +84,8 @@ template <typename Key, typename Val> class LruCache
         auto it = _cache_items_map.find(key);
         if (it == _cache_items_map.end())
         {
-            throw std::range_error("There is no such key in cache");
+            value_t val = underflow();
+            put(key, val);
         }
         else
         {
@@ -63,9 +104,22 @@ template <typename Key, typename Val> class LruCache
         return _cache_items_map.size();
     }
 
-  private:
-    using ListIter = typename std::list<std::pair<Key, Val>>::iterator;
-    size_t m_cap;
-    std::list<std::pair<Key, Val>> m_list;
-    std::unordered_map<Key, ListIter> m_map;
+private:
+    virtual bool overflow(const key_t &key, const value_t &value)
+    {
+    }
+
+    virtual value_t underflow(const key_t &key)
+    {
+        throw llama::Exception{"lru_cache: Key not found."};
+    }
+
+private:
+    std::list<key_value_pair_t> _cache_items_list;
+    std::unordered_map<key_t, list_iterator_t> _cache_items_map;
+    size_t _max_size;
 };
+
+} // namespace llama
+
+#endif /* _LRUCACHE_HPP_INCLUDED_ */
