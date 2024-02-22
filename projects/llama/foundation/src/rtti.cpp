becf7b7eca7e98284b069ce71011735c6b515695
yangerhang <yangerhang@kingsoft.com>
2024-02-22 18:06:39 +0800

    避免静态属性，因为MSVC不会导出属性，只会导出函数!

--- projects/llama/foundation/src/rtti.cpp
+++ projects/llama/foundation/src/rtti.cpp
@@ -1,6 +1,13 @@
 #include "foundation/rtti.h"
+#include <array>
 
 namespace llama
 {
+
+RttiContext &RttiContext::GetDefaultInstance()
+{
+    static RttiContext context{false};
+    return context;
+}
 
 } // namespace llama
