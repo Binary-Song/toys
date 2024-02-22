e00924b40aad8d59b23d68ac909f741ff2fbb6a8
yangerhang <yangerhang@kingsoft.com>
2024-02-22 10:12:50 +0800

    暂时禁用path.h

--- projects/llama/foundation/include/foundation/path.h
+++ projects/llama/foundation/include/foundation/path.h
@@ -1,53 +1,55 @@
-#pragma once
-#include "codec.h"
-#include "foundation/enums.h"
-#include "foundation/exceptions.h"
-#include "foundation/path.h"
-#include <fstream>
-#include <string>
-#include <string_view>
-#include <vector>
-namespace llama
-{
+// #pragma once
+// #include "codec.h"
+// #include "foundation/enums.h"
+// #include "foundation/exceptions.h"
+// #include "foundation/path.h"
+// #include <fstream>
+// #include <string>
+// #include <string_view>
+// #include <vector>
+// namespace llama
+// {
 
-/// 相对路径。
-/// 相对路径需要和绝对路径组合才能形成完整路径。只能通过绝对路径打开文件。
-class RelativePath
-{
-  public:
-    RelativePath(std::string_view path, ReadPathOption opt = ReadPathOption::None);
+// enum class Read
 
-    RelativePath(std::wstring_view path, ReadPathOption opt = ReadPathOption::None) : RelativePath{ToUtf8(path), opt}
-    {
-    }
+// /// 相对路径。
+// /// 相对路径需要和绝对路径组合才能形成完整路径。只能通过绝对路径打开文件。
+// class RelativePath
+// {
+//   public:
+//     RelativePath(std::string_view path, ReadPathOption opt = ReadPathOption::None);
 
-    RelativePath(std::u16string_view path, ReadPathOption opt = ReadPathOption::None) : RelativePath{ToUtf8(path), opt}
-    {
-    }
+//     RelativePath(std::wstring_view path, ReadPathOption opt = ReadPathOption::None) : RelativePath{ToUtf8(path), opt}
+//     {
+//     }
 
-    std::string ToString(WritePathOption opt = WritePathOption_NativeSep) const
-    {
-        std::vector<std::string const *> stack;
-        std::string result;
-        result.reserve(5 * m_components.size());
-        for (std::string const &component : m_components)
-        {
-            if (+(opt & WritePathOption::WindowsSep))
-            {
-                result.append(component);
-            }
-        }
-    }
+//     RelativePath(std::u16string_view path, ReadPathOption opt = ReadPathOption::None) : RelativePath{ToUtf8(path), opt}
+//     {
+//     }
 
-  private:
-    std::vector<std::string> m_components;
-};
+//     std::string ToString(WritePathOption opt = WritePathOption_NativeSep) const
+//     {
+//         std::vector<std::string const *> stack;
+//         std::string result;
+//         result.reserve(5 * m_components.size());
+//         for (std::string const &component : m_components)
+//         {
+//             if (+(opt & WritePathOption::WindowsSep))
+//             {
+//                 result.append(component);
+//             }
+//         }
+//     }
 
-/// 绝对路径
-class Path
-{
-  public:
-    Path(std::string_view path, ReadPathOption opt = ReadPathOption::None);
-};
+//   private:
+//     std::vector<std::string> m_components;
+// };
 
-} // namespace llama
+// /// 绝对路径
+// class Path
+// {
+//   public:
+//     Path(std::string_view path, ReadPathOption opt = ReadPathOption::None);
+// };
+
+// } // namespace llama
