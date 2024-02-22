5ab9f134f692d559d198392500165e966f910ec7
yangerhang <yangerhang@kingsoft.com>
2024-02-22 10:10:41 +0800

    将rtti接入pointers.h，添加相应接口

---
+++ projects/llama/foundation/include/foundation/interfaces/rtti.h
@@ -0,0 +1,113 @@
+#pragma once
+#include "../rtti.h"
+#include <string_view>
+namespace llama
+{
+
+/// 表示该类型的对象具有"运行时类型信息"(rtti)。
+/// 实现本接口的作用是让对象支持向下转型（基类转派生类）。
+///
+/// 一般不需要自己实现这个接口。使用 LLAMA_RTTI 来自动实现。
+/// LLAMA_RTTI 的语法是：
+///
+/// 1. LLAMA_RTTI(当前类型) 或
+/// 2. LLAMA_RTTI(当前类型, 基类1 , ..., 基类n )
+///
+/// LLAMA_RTTI 需要在 `class` 声明内调用。当前类型就是 LLAMA_RTTI 所在的类型。
+///
+/// 基类可以是直接基类或者间接基类。 这些基类本身必须：
+///
+/// 1. 直接或间接派生自IRtti
+/// 2. 用 LLAMA_RTTI 添加了元信息。
+///
+/// 语法 (2) 指定的每个基类都对应继承图中一条从当前类型指向基类的有向边。
+/// 如果可以沿着这些边从类型 A 到达类型 B ，则可以用 IRtti::Cast 从 B* 向下转型到
+/// A* 。
+///
+/// 例：
+/// ```
+/// class GrandParent : public virtual IRtti
+/// {
+///     LLAMA_RTTI(GrandParent)
+/// };
+/// class Parent : public GrandParent
+/// {
+///     LLAMA_RTTI(Parent, GrandParent)
+/// };
+/// class Child : public Parent
+/// {
+///     LLAMA_RTTI(Child, Parent)
+/// };
+/// void test()
+/// {
+///     RttiContext ctx;
+///     Child child;
+///     GrandParent &parent = child;
+///     Child *child2 = parent.Cast<Child>(ctx);
+/// 	EXPECT_EQ(child, child2);
+/// }
+/// ```
+///
+/// 由于存在开销，大部分类型不包含 RTTI ，可根据需要添加。
+/// 增加 LLAMA_RTTI 不会影响对象的大小和结构、不会增加 dll 的导出符号。
+///
+class IRtti
+{
+public:
+    virtual ~IRtti() = default;
+
+private:
+    /// 返回的指针的类型为 “最派生” 且标记了 LLAMA_RTTI 的类型。
+    /// @note 不要手动实现本函数，用 LLAMA_RTTI 。
+    virtual const void *GetSelf_IRtti() const = 0;
+
+    /// 应返回 “最派生” 且标记了 LLAMA_RTTI 的类型ID。
+    /// @note 不要手动实现本函数，用 LLAMA_RTTI 。
+    virtual TypeId GetTypeId_IRtti() const = 0;
+
+public:
+    void *GetSelf()
+    {
+        return const_cast<void *>(const_cast<const IRtti *>(this)->GetSelf_IRtti());
+    }
+
+    const void *GetSelf() const
+    {
+        return GetSelf_IRtti();
+    }
+
+    TypeId GetTypeId() const
+    {
+        return GetTypeId_IRtti();
+    }
+
+    /// 利用 LLAMA_RTTI 进行动态类型转换。支持 upcast 、 downcast 和 cross-cast 。
+    /// @exception 如果转换失败则抛出异常。
+    template <typename Dst> p<Dst> Cast(RttiContext &context)
+    {
+        return context.Cast(GetSelf(), GetTypeId(), rtti_trait<Dst>::id).unwrap().template cast_static<Dst>();
+    }
+
+    /// 利用 LLAMA_RTTI 进行动态类型转换。支持 upcast 、 downcast 和 cross-cast
+    /// @exception 如果转换失败则抛出异常。
+    template <typename Dst> p<const Dst> Cast(RttiContext &context) const
+    {
+        return context.Cast(GetSelf(), GetTypeId(), rtti_trait<Dst>::id).unwrap().template cast_static<Dst>();
+    }
+
+    /// 利用 LLAMA_RTTI 进行动态类型转换。支持 upcast 、 downcast 和 cross-cast
+    /// 如果转换失败返回空指针。
+    template <typename Dst> np<Dst> TryCast(RttiContext &context)
+    {
+        return context.Cast(GetSelf(), GetTypeId(), rtti_trait<Dst>::id).template cast_static<Dst>();
+    }
+
+    /// 利用 LLAMA_RTTI 进行动态类型转换。支持 upcast 、 downcast 和 cross-cast
+    /// 如果转换失败返回空指针。
+    template <typename Dst> np<const Dst> TryCast(RttiContext &context) const
+    {
+        return context.Cast(GetSelf(), GetTypeId(), rtti_trait<Dst>::id).template cast_static<Dst>();
+    }
+};
+
+} // namespace llama
--- projects/llama/foundation/include/foundation/rtti.h
+++ projects/llama/foundation/include/foundation/rtti.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "misc.h"
 #include <cstdint>
 #include <functional>
 #include <list>
@@ -8,12 +9,60 @@
 #include <string>
 #include <string_view>
 
+#define LLAMA_RTTI_UPCAST_BEGIN(T)                                                                                     \
+private:                                                                                                               \
+    friend class rtti_trait<T>;                                                                                        \
+                                                                                                                       \
+private:                                                                                                               \
+    inline static char s_typeId[] = #T;                                                                                \
+                                                                                                                       \
+private:                                                                                                               \
+    inline static unsigned char s_add_default_cast = []() {
+
+#define LLAMA_RTTI_UPCAST_ENTRY(From, To) ::llama::AddDefaultCast<From, To>();
+
+#define LLAMA_RTTI_UPCAST_END                                                                                          \
+    return (unsigned char)0;                                                                                           \
+    }                                                                                                                  \
+    ();
+
+#define LLAMA_RTTI_OVERRIDE_FUNCS(T)                                                                                   \
+private:                                                                                                               \
+    const void *GetSelf_IRtti() const override                                                                         \
+    {                                                                                                                  \
+        static_assert(std::is_same<std::decay<decltype(*this)>::type, T>::value,                                       \
+                      "first arg to LLAMA_RTTI must be the current type.");                                            \
+        return this;                                                                                                   \
+    }                                                                                                                  \
+                                                                                                                       \
+    ::llama::TypeId GetTypeId_IRtti() const override                                                                   \
+    {                                                                                                                  \
+        return &s_typeId[0];                                                                                           \
+    }
+
+/// @copydoc llama::IRtti
+#define LLAMA_RTTI(...) LLAMA_VA_SELECT(LLAMA_RTTI, __VA_ARGS__) LLAMA_RTTI_UPCAST_END
+// clang-format off
+#define LLAMA_RTTI_1(T) LLAMA_RTTI_OVERRIDE_FUNCS(T)  LLAMA_RTTI_UPCAST_BEGIN(T) 
+#define LLAMA_RTTI_2(T, B1)                             LLAMA_RTTI_1(T)                             LLAMA_RTTI_UPCAST_ENTRY(T, B1)
+#define LLAMA_RTTI_3(T, B1, B2)                         LLAMA_RTTI_2(T, B1)                         LLAMA_RTTI_UPCAST_ENTRY(T, B2)
+#define LLAMA_RTTI_4(T, B1, B2, B3)                     LLAMA_RTTI_3(T, B1, B2)                     LLAMA_RTTI_UPCAST_ENTRY(T, B3)
+#define LLAMA_RTTI_5(T, B1, B2, B3, B4)                 LLAMA_RTTI_4(T, B1, B2, B3)                 LLAMA_RTTI_UPCAST_ENTRY(T, B4)
+#define LLAMA_RTTI_6(T, B1, B2, B3, B4, B5)             LLAMA_RTTI_5(T, B1, B2, B3, B4)             LLAMA_RTTI_UPCAST_ENTRY(T, B5)
+#define LLAMA_RTTI_7(T, B1, B2, B3, B4, B5, B6)         LLAMA_RTTI_6(T, B1, B2, B3, B4, B5)         LLAMA_RTTI_UPCAST_ENTRY(T, B6)
+#define LLAMA_RTTI_8(T, B1, B2, B3, B4, B5, B6, B7)     LLAMA_RTTI_7(T, B1, B2, B3, B4, B5, B6)     LLAMA_RTTI_UPCAST_ENTRY(T, B7)
+#define LLAMA_RTTI_9(T, B1, B2, B3, B4, B5, B6, B7, B8) LLAMA_RTTI_8(T, B1, B2, B3, B4, B5, B6, B7) LLAMA_RTTI_UPCAST_ENTRY(T, B8)
+// clang-format on
+
 namespace llama
 {
+
+/// 类型ID，一个静态的字符串。比较时比较其内容而不是地址。
 using TypeId = std::string_view;
+
 class RttiContext
 {
-  public:
+public:
     using CastFunction = void *(*)(void *) noexcept;
 
     RttiContext() : m_casts(s_casts)
@@ -32,7 +81,7 @@ class RttiContext
         return dst_map.insert({dst, fn}).second;
     }
 
-    void *Cast(void *src_obj, TypeId src, TypeId dst)
+    np<void> Cast(void *src_obj, TypeId src, TypeId dst)
     {
         if (void *result = DirectCast(src_obj, src, dst))
         {
@@ -46,14 +95,16 @@ class RttiContext
         return nullptr;
     }
 
-    const void *Cast(const void *src_obj, TypeId src, TypeId dst)
+    np<const void> Cast(const void *src_obj, TypeId src, TypeId dst)
     {
         return Cast(const_cast<void *>(src_obj), src, dst);
     }
 
-  private:
+private:
     void *DirectCast(void *src_obj, TypeId src, TypeId dst)
     {
+        if (src == dst)
+            return src_obj;
         auto src_iter = m_casts.find(src);
         if (src_iter != m_casts.end())
         {
@@ -106,7 +157,7 @@ class RttiContext
         return nullptr;
     }
 
-  private:
+private:
     /// m_casts[src][dst] -> cast_fn
     std::map<TypeId, std::map<TypeId, CastFunction>> m_casts;
     LLAMA_API(foundation) inline static std::map<TypeId, std::map<TypeId, CastFunction>> s_casts;
@@ -114,8 +165,14 @@ class RttiContext
 
 template <typename T> class rtti_trait
 {
-  public:
+public:
     inline static constexpr const char *id = &T::s_typeId[0];
 };
+
+template <typename From, typename To> inline void AddDefaultCast()
+{
+    RttiContext::AddDefaultCast(rtti_trait<From>::id, rtti_trait<To>::id,
+                                [](void *from) noexcept -> void * { return static_cast<To *>((From *)from); });
+}
 
 } // namespace llama
